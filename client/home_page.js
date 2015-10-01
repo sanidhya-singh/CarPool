5aa61b8336f2d7e336a2c0520f4e7f927f25eeef
sanidhya-singh <sanidhya235@gmail.com>
2015-10-01 21:05:29 +0530

    Minor Update
    - fixed header (now looks like what I envisioned)
    - fixed bug with the "Welcome to CarPool" check, now loops once instead
    of indefinitely

--- client/home_page.js
+++ client/home_page.js
@@ -6,5 +6,14 @@ Template.homePage.onRendered(function() {
 
 function foo() {
   $('#text-shape').shape('flip up');
-  setTimeout(foo, 3500);
+  setTimeout(foo2, 3500);
+}
+
+function foo2() {
+  $('#text-shape').shape('flip up');
+  setTimeout(foo3, 3500);
+}
+
+function foo3() {
+  $('#text-shape').shape('flip down');
 }
